48162627c991a9cb7eba6a7bdf170f99340665f9
Scotty <william.scott.peery@gmail.com>
2016-10-25 21:37:37 -0500

    more comments

--- Assignment10/src/Introspection_Reflection/Driver.java
+++ Assignment10/src/Introspection_Reflection/Driver.java
@@ -12,29 +12,26 @@ public class Driver {
     	// Create the object that will be accessed via reflection
     	NetworkService target = new NetworkService();
     	
-    	Class reflectionTargetClass = target.getClass();
-    	
+    	Class <? extends NetworkService> reflectionTargetClass = target.getClass();
+    	//
     	Field fields[] = reflectionTargetClass.getDeclaredFields();
     	System.out.println("\nFields:");
-    	
+    	//loop to print out all fields in NetworkService
 	    for (Field field : fields) {
 	    	System.out.println(field.getName() + " is: "
 	    			+ field.toGenericString());
 	    }
-	    
+	//access field     
 	Field notSecretStringAttribute = reflectionTargetClass.getDeclaredField("machineName");
 	// The filed is private. In order to read/write the value we have to change accessibility to public.
 	notSecretStringAttribute.setAccessible(true);
-	    
+	//string to get value of string from field   
 	String notSecretStringValue = (String)notSecretStringAttribute.get(target);
-	    
+	//print out the value of the string    
 	System.out.println("String attribute value is: " + notSecretStringValue);
 	//reset value of target    
 	notSecretStringAttribute.set(target, "aws.com");
-	//use changed target value to make network connection    
+	//use changed target value to make NetworkService connection    
         target.connect();
     }
 }
-
-
-
